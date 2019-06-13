e568777ca7ae55bcaca1533d00e99065ee0b136f
eterry28 <eric.terry.kc@gmail.com>
2019-06-13 08:18:18 -0500

    Add files via upload
    Added read single function.

--- bookmarks/models/Bookmarks.php
+++ bookmarks/models/Bookmarks.php
@@ -1,5 +1,5 @@
 <?php
-class Bookmarks 
+class Bookmark 
 {
     // database info
     private $conn;
@@ -35,6 +35,38 @@ class Bookmarks
         $stmt = $this->conn->prepare($query);
 
         $stmt->execute();
+
+        return $stmt;
+    }
+
+    // get all bookmarks
+    public function read_single()
+    {
+        $query = 'SELECT 
+            b.bookmark_id,
+            b.parent_id,
+            b.title,
+            b.url,
+            b.create_date
+            FROM 
+            bookmarks b
+            left join bookmarks p on b.parent_id = p.bookmark_id
+            WHERE b.bookmark_id = ?
+            LIMIT 0,1';           
+
+        $stmt = $this->conn->prepare($query);
+
+        $stmt->bindParam(1, $this->bookmark_id);
+
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        $this->bookmark_id = $row['bookmark_id'];
+        $this->parent_id = $row['parent_id'];
+        $this->title = $row['title'];
+        $this->url = $row['url'];
+        $this->create_date = $row['create_date'];
 
         return $stmt;
     }
