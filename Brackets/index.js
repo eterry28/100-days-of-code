b282984e773da7a20f7da6447bc6351e06ee901c
Eric Terry <eric.terry.kc@gmail.com>
2020-03-31 20:23:42 -0500

    Create index.js

---
+++ Brackets/index.js
@@ -0,0 +1,6 @@
+function setSelected()
+{
+    var bracketSelectorValue = document.getElementById('bracket-selector').value;
+    //alert(bracketSelectorValue);
+    document.getElementById('bracket-selector').value=bracketSelectorValue;
+}
