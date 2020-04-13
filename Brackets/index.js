3158e497a72c3e78edf7e6a03c3520063b7ce1e9
Eric Terry <eric.terry.kc@gmail.com>
2020-04-13 15:32:58 -0500

    small update

--- Brackets/index.js
+++ Brackets/index.js
@@ -1,6 +1,28 @@
+
+
+
+
 function setSelected()
 {
     var bracketSelectorValue = document.getElementById('bracket-selector').value;
     //alert(bracketSelectorValue);
     document.getElementById('bracket-selector').value=bracketSelectorValue;
+}
+
+
+function vote(group, id)
+{
+    alert("group => " + group + ": id => " + id);
+}
+
+function Contender(title, url, position)
+{
+    this.title = title;
+    this.url = url;
+    this.position = position;
+
+    this.vote() = function()
+    {
+        alert("voted for " + this.title);
+    }
 }
