e2fe861bee61288f9bcd5ce47cdc6f4124a5982f
Eric Terry <eric.terry@intouchsol.com>
2020-04-14 19:25:00 -0500

    small update to bracket javascript

--- Brackets/shows.js
+++ Brackets/shows.js
@@ -15,18 +15,35 @@ const request = async () => {
 request();
 
 function displayRound1(sortedShows) {
-  for (i in sortedShows) {
+  let nextId = 32;
+  for (i=0; i<32; i++) {
+    
     let Contender = sortedShows[i];
     let contenderId = "#contender"+i;
     let listenId = "#listen"+i;
-    ContenderMatch(Contender, contenderId, listenId);
+    let voteId = "vote"+i;
+    ContenderMatch(Contender, contenderId, listenId, voteId, nextId);
+
+    if(i%2 == 0){
+      nextId++;
+    }
+
   }
 }
 
-function ContenderMatch(object, contenderId, listenId) {
-  //console.table(object, contenderId, listenId);
+function ContenderMatch(object, contenderId, listenId, voteId, nextId) {
+  console.table(object, contenderId, listenId, voteId, nextId);
   document.querySelector(contenderId).innerHTML = object.name;
   document.querySelector(listenId).href = object.url;
-  
+  document.getElementById(voteId).addEventListener("click", function(){ 
+    vote(object, nextId);
+  });
+}
+
+function vote(contender, nextId)
+{
+  alert("Voting for: " + contender.name + ". Goes to: " + nextId);
+  document.querySelector("contender" + nextId).innerHTML = contender.name;
+  document.querySelector("listen" + nextId).href = contender.url;
 }
 
